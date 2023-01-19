2a884311aba9eda521c2d79f2f89597d98b922a2
sudhi777 <87425107+sudhi7seas@users.noreply.github.com>
2023-01-19 07:47:07 +0000

    Automatic login and then scrapes the dynamic value and prints it in console.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+#one we give the login credentials, we click "enter key" and hence importing key is required
+from selenium.webdriver.common.keys import Keys
+import time
 
 def get_driver():
 
@@ -23,17 +26,33 @@ def get_driver():
   options.add_argument("disable-blink-features=AutomationControlled")
   
   driver = webdriver.Chrome(options=options)
-  driver.get("http://automated.pythonanywhere.com")
+  driver.get("http://automated.pythonanywhere.com/login/")
   return driver
+
+#to get only the dynamic value and remove the text associated with it
+def clean_text(text):
+  output = float(text.split(": ")[1])
+  return output
 
 def main():
   driver = get_driver()
   
   #find element by xpath is not supported and hence here find element has been 
   #used with "by" and "value" arguments.
-  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
-  #to get the actual text in the console, "element.text" is required
-  return element.text
-  
+  driver.find_element(by="id", value="id_username").send_keys("automated")
+  time.sleep(2)
+  driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+  time.sleep(2)
+  #to click on "HOME" button after login, when we inspected it we found that it is of type xpath
+  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+  time.sleep(5)
+
+  #to scrape the dynamic value after login
+  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+
+  return clean_text(element.text)
+  #to print the url name in the console
+  print(driver.current_url)
+
 print(main())
 
